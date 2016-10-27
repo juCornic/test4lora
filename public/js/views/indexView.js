ad287b52327ed236fab0964ba06ad05ca2f090bf
BeDartiguelongue <b.dartiguelongue@kimo.fr>
2016-10-27 13:34:47 +0200

    correct device click

--- public/js/views/indexView.js
+++ public/js/views/indexView.js
@@ -1,11 +1,20 @@
 ﻿
-	var devList = [];
-	
     var indexView = React.createClass({
+	
+		deviceList: [],
 	
 		deviceClick: function(sender)
 		{
+			var selectedEUI = sender.target.innerText.split(" - ")[1];
 			
+			for(var i = 0 ; i < this.deviceList.length ; i++)
+			{
+				if (this.deviceList[i].devEUI == selectedEUI)
+				{
+					this.setState({deviceIndex: i});
+					break;
+				}
+			}
 		},
 	
 		getInitialState: function()
@@ -15,7 +24,11 @@
 	
         render: function ()
         {
+			var height = (document.body.clientHeight).toString() + "px";
+            var width = "300px"; 
+			var view = this;
 			var req = new XMLHttpRequest();
+			
             req.onload = reqListener;
             req.open('get', 'https://liveobjects.orange-business.com/api/v0/vendors/lora/devices?status=ACTIVATED&sort=name', false);
 			req.setRequestHeader("X-API-Key", "310def0e53f04c0b814fa8f3c05124fb");
@@ -23,25 +36,22 @@
 
             function reqListener(e)
             {
-                devList = JSON.parse(this.responseText).data;
+                view.deviceList = JSON.parse(this.responseText).data;
             }
-			
-			var height = (document.body.clientHeight).toString() + "px";
-            var width = "300px"; 
 			
             return React.createElement("div", null,
                 React.createElement(header, { text: "Test Lora" }),
-				React.createElement(deviceList, { device: devList[this.state.deviceIndex] }),
+				React.createElement(deviceList, { device: this.deviceList[this.state.deviceIndex] }),
 					React.createElement("div", { style: { height: height, width: width, background: "#EEEEEE" }},
 
 						React.createElement("br", null, null),
 						
-						devList.map(function (device)
+						this.deviceList.map(function (device)
                                   {
-									  return React.createElement("div", null,
-										React.createElement("a", { onClick: this.deviceClick, href: "#", style:{ paddingLeft: "5px"} }, device.name + " - " + device.devEUI),
-										React.createElement("br", null, null),
-										React.createElement("br", null, null)
+									  return React.createElement("ul", null,
+										React.createElement("li", { onClick: view.deviceClick }, 
+											React.createElement("a", { href:"#"}, device.name + " - " + device.devEUI)
+										)
 									  );
 									  
 								  })
