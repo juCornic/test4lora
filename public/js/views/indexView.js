6b704c9bae3a3cbb0b8063e764c2180c8dd56d8f
BeDartiguelongue <b.dartiguelongue@kimo.fr>
2016-10-27 12:02:07 +0200

    Correct device list display

--- public/js/views/indexView.js
+++ public/js/views/indexView.js
@@ -1,13 +1,23 @@
 ﻿
 	var devList = [];
-
+	
     var indexView = React.createClass({
+	
+		deviceClick: function(sender)
+		{
+			
+		},
+	
+		getInitialState: function()
+		{
+			return { deviceIndex: 0};
+		},
 	
         render: function ()
         {
 			var req = new XMLHttpRequest();
             req.onload = reqListener;
-            req.open('get', 'https://liveobjects.orange-business.com/api/v0/vendors/lora/devices?status=ACTIVATED&sort=-name', false);
+            req.open('get', 'https://liveobjects.orange-business.com/api/v0/vendors/lora/devices?status=ACTIVATED&sort=name', false);
 			req.setRequestHeader("X-API-Key", "310def0e53f04c0b814fa8f3c05124fb");
             req.send();
 
@@ -21,14 +31,18 @@
 			
             return React.createElement("div", null,
                 React.createElement(header, { text: "Test Lora" }),
-				React.createElement(deviceList, { device: devList[0] }),
+				React.createElement(deviceList, { device: devList[this.state.deviceIndex] }),
 					React.createElement("div", { style: { height: height, width: width, background: "#EEEEEE" }},
 
-						React.createElement("br", null, null),						
+						React.createElement("br", null, null),
 						
 						devList.map(function (device)
                                   {
-									  return React.createElement("a", { href: "#" }, device.name + " - " + device.devEUI)
+									  return React.createElement("div", null,
+										React.createElement("a", { onClick: this.deviceClick, href: "#", style:{ paddingLeft: "5px"} }, device.name + " - " + device.devEUI),
+										React.createElement("br", null, null),
+										React.createElement("br", null, null)
+									  );
 									  
 								  })
 					)
